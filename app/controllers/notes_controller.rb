e53ab48a55b4a463dd02f789fb02f65b59cff472
bendoane <ben.doane@gmail.com>
2015-12-07 14:36:10 -0500

    setting db up

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,15 @@
 class NotesController < ApplicationController
+
+
+def new
+end
+
+def create
+  @note = Note.new
+  @note.save
+end
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Note.create(title: 'First Note', body: "It's full of wonder.", tags: (name: 'cool', name: 'nifty', name: 'neato'))
+Note.create(title: 'This second Note', body: "Wow. So amaze. Much text.", tags: (name: 'wow', name: 'amaze', name: 'doge'))
+Note.create(title: "This third one", body: "But dat text doh, u kno?", tags: (name: "bruh", name: "bro", name: "dude"))
+Note.create(title: "Fourth Meal", body: "One chicken cheesy gordita crunch", tags:(name: "yum", name: "taco_bell", name: "amaze"))
+Note.create(title: "Fifth Wheel" body: "Nobody likes me. Everybody hates me. I'll eat dirt", tags: (name:"sad", name:"forever_alone", name:"boohoo"))
